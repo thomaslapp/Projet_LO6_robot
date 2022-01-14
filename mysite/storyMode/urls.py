d9ca5255e0f9bd8ce375a2604c2719208fccc5d2
w.chalabi <orimius.psn@gmail.com>
2022-01-14 17:07:49 +0100

    Fixed route bug

--- mysite/storyMode/urls.py
+++ mysite/storyMode/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.mode_histoire, name='mode_histoire'),
+    path('storyOne/', include('storyOne.urls')),
 ]
